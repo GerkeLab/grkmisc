959936743174b380852d4f9eecc69c29e9b888d4
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-05-30 11:14:57 -0400

    Add read_[ct]sv_filtered()

---
+++ R/read_filtered.R
@@ -0,0 +1,46 @@
+#' Read TSV and Filter on the Fly
+#'
+#' @param file <chr> Filename to read in
+#' @param filter_expression <bare> The expression passed to `dplyr::filter()`
+#' @inheritDotParams readr::read_tsv_chunked
+#' @export
+read_tsv_filtered <- function(file, filter_expression, ...) {
+  stopifnot(requireNamespace("dplyr", quietly = TRUE),
+            requireNamespace("readr", quietyl = TRUE))
+
+  filter_expression <- rlang::enexpr(filter_expression)
+
+  filter_by_id <- function(.data, pos) {
+    dplyr::filter(.data, !!filter_expression)
+  }
+
+  readr::read_tsv_chunked(
+    file,
+    readr::DataFrameCallback$new(filter_by_id),
+    ...
+  )
+}
+
+#' Read CSV and Filter on the Fly
+#'
+#' @param file <chr> Filename to read in
+#' @param filter_expression <bare> The expression passed to `dplyr::filter()`
+#' @inheritDotParams readr::read_csv_chunked
+#' @export
+read_csv_filtered <- function(file, filter_expression, ...) {
+  stopifnot(requireNamespace("dplyr", quietly = TRUE),
+            requireNamespace("readr", quietyl = TRUE))
+
+  filter_expression <- rlang::enexpr(filter_expression)
+
+  filter_by_id <- function(.data, pos) {
+    dplyr::filter(.data, !!filter_expression)
+  }
+
+  readr::read_csv_chunked(
+    file,
+    readr::DataFrameCallback$new(filter_by_id),
+    ...
+  )
+}
+
