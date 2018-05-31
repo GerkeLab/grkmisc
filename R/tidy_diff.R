ddca9af279e936fb2ace211d4a7c66a5bf8fec4f
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-05-31 15:52:06 -0400

    Add initial summary() method

--- R/tidy_diff.R
+++ R/tidy_diff.R
@@ -11,34 +11,64 @@
 #' @export
 tidy_diff <- function(x, y, .id = NULL) {
   # x <- arrange(x, .id)
+  x_name = rlang::quo_name(rlang::enquo(x))
+  y_name = rlang::quo_name(rlang::enquo(y))
   if (!is.null(.id)) {
     ids <- select(x, .id)
     x <- select(x, -.id)
     y <- select(y, -.id)
   } else ids <- NULL
+
+  # De-factorize into characters
+  x <- mutate_if(x, is.factor, as.character)
+  y <- mutate_if(y, is.factor, as.character)
+
   xt <- purrr::map_dfr(x, ~ data_frame(value = list(.)), .id = "variable")
   yt <- purrr::map_dfr(y, ~ data_frame(value = list(.)), .id = "variable")
-  z <- left_join(xt, yt, by = "variable") %>%
+  z <- full_join(xt, yt, by = "variable") %>%
     mutate(
-      misses = purrr::map2(value.x, value.y, ~ which(..1 != ..2)),
+      misses     = purrr::map2(value.x, value.y, ~ not_equal(..1, ..2)),
       miss_count = purrr::map_int(misses, length),
-      state = ifelse(miss_count == 0, "same", "changed"),
+      miss_count = ifelse(purrr::map_lgl(value.x, is.null), NA, miss_count),
+      miss_count = ifelse(purrr::map_lgl(value.y, is.null), NA, miss_count),
+      state      = ifelse(miss_count == 0, "same", "changed"),
+      state      = case_when(
+        miss_count > 0    ~ "changed",
+        is.na(miss_count) ~ "unique",
+        miss_count == 0   ~ "same",
+        TRUE ~ "other"
+      )
     ) %>%
     split(.$state)
 
+  # TODO: catch no changes
   z_tidy_diff <- z$changed
   z_tidy_diff$value.x <- purrr::map2(z$changed$value.x, z$changed$misses, function(x, y) x[y])
   z_tidy_diff$value.y <- purrr::map2(z$changed$value.y, z$changed$misses, function(x, y) x[y])
   z_tidy_diff <- z_tidy_diff %>%
     select(-miss_count, -state) %>%
-    tidyr::unnest() %>%
-    rename(miss_index = misses)
+    split(.$variable) %>%
+    purrr::map(~ {
+      tidyr::unnest(.) %>%
+        rename(miss_index = misses)
+    })
+
+  z_tidy_diff <- purrr::map(z_tidy_diff, function(z_td) {
+    bind_cols(z_td, x[z_td$miss_index,  z$same$variable])
+  })
+
+  meta <- list()
+  meta$names <- c(x = x_name, y = y_name)
+  meta$dims <- purrr::map(list(x = x, y = y), ~ dim(.))
+  meta$colnames <- purrr::map(list(x = x, y = y), ~ colnames(.))
 
   structure(list(
-    tidy = z_tidy_diff,
+    tidy    = z_tidy_diff,
     changed = z$changed,
-    same = z$same,
-    ids = ids
+    same    = z$same,
+    unique  = z$unique,
+    ids     = ids,
+    meta    = meta
   ), class = "tidy_diff")
 }
 
@@ -50,10 +80,14 @@ print.tidy_diff <- function(z, n = 5) {
     )
   }
   z_tidy <- z$tidy %>%
-    split(.$variable) %>%
+  {
+    x <- .
+    if (inherits(x, "data.frame")) split(x, x$variable) else x
+  } %>%
     purrr::map(
       function(x) {
-        tidyr::gather(x, set, value, contains("value")) %>%
+        select(x, variable, contains("value"), miss_index) %>%
+          tidyr::gather(set, value, contains("value")) %>%
           tidyr::spread(miss_index, value) %>%
           mutate(set = sub("value\\.", "", set))
       }
@@ -98,3 +132,66 @@ plot.tidy_diff <- function(z) {
         ggplot2::labs(x = "Column", y = "Row", fill = "Diff.")
     }
 }
+
+not_equal <- function(x, y) {
+  # Check class of x and y and if not the same then hope for the best
+  if (!identical(class(x), class(y))) class(y) <- class(x)
+  switch(
+    class(x),
+    "double" = which(abs(x - y) > .Machine$double.eps),
+    which(x != y)
+  )
+}
+
+#' @inheritParams glue glue glue_collapse
+summary.tidy_diff <- function(z) {
+  # 1. Compare dims
+  # 2. Column names
+  # 3. Column order
+  # 4. Column type
+  # 5. Number of differing columns
+  # 6. Number of differing rows
+
+  dims <- purrr::map_dfr(setNames(z$meta$dims, z$meta$names),
+    ~ data_frame(rows = .[1], cols = .[2]), .id = "set")
+
+  cli::cat_rule("Comparison Summary")
+  print(dims)
+  cli::cat_line()
+
+  x_uniq <- setdiff(z$meta$colnames$x, z$meta$colnames$y)
+  y_uniq <- setdiff(z$meta$colnames$y, z$meta$colnames$x)
+
+  trunc_bullet <- function(x, ...) {
+    if (x > getOption("width")) x <- paste0(strtrim(x, getOption("width") - 3), "...\033[0m")
+    cli::cat_bullet(x, ...)
+  }
+  style_vars <- function(x) pillar::style_subtle(glue_collapse(glue("`{x}`"), sep = ", "))
+
+  cat_thing_w_count <- function(thing, values, styler = crayon::bold) {
+    if (!length(values)) return(invisible())
+    plural <- if (length(values) > 1) "s" else ""
+    trunc_bullet(glue(
+      "'{thing}' has {styler(length(values))} unique column{plural}: ",
+      style_vars(values)
+    ))
+  }
+  cat_thing_w_count(z$meta$names["x"], x_uniq)
+  cat_thing_w_count(z$meta$names["y"], y_uniq)
+  if (!length(x_uniq) && !length(y_uniq)) trunc_bullet(
+    glue(
+      "\'{z$meta$names['x']}\' and \'{z$meta$names['y']}\' have the same columns: ",
+      style_vars(z$meta$colnames$x)
+    )
+  )
+
+  # TODO: Catch no changes
+  trunc_bullet(glue(
+    "There are {crayon::bold(sum(z$changed$miss_count))} differing values ",
+    "across {crayon::bold(length(unique(unlist(z$changed$misses))))} rows"
+  ))
+
+}
+
+
+test <- function(x, y) browser()
