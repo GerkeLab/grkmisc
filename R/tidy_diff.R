86f516bc64604813d88986f65d78e75050cb984d
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-06-13 12:39:13 -0400

    Add alignment, handle mismatched rows, handle no diff

--- R/tidy_diff.R
+++ R/tidy_diff.R
@@ -3,27 +3,39 @@
 #' Finds row-wise differences in two data frames that are assumed to be
 #' pre-arranged.
 #'
-#' @param x <tbl|df> Reference data frame
-#' @param y <tbl|df> Comparison data frame
-#' @param ignore <chr> ID column(s) that are ignored from the differencing
+#' @param x `<tbl|df>` Reference data frame
+#' @param y `<tbl|df>` Comparison data frame
+#' @param ignore `<chr:NULL>` ID column(s) that are ignored from the differencing
+#' @param group_vars `<chr:ignore>` Column names of grouping variables for alignment,
+#'   takes default from `ignore`.
+#' @param align `<lgl:FALSE>` Should alignmment be performed based on the
+#'   grouping variables or the `group_vars` parameter?
 #' @return Object of class `tidy_diff` that can be printed via `print(obj)` or
 #'   plotted with [ggplot2] via `plot(obj)`.
 #' @export
-tidy_diff <- function(x, y, ignore = NULL) {
+tidy_diff <- function(x, y, ignore = NULL, group_vars = ignore, align = FALSE) {
   # x <- arrange(x, .id)
   x_name = rlang::quo_name(rlang::enquo(x))
   y_name = rlang::quo_name(rlang::enquo(y))
-
-  if (!is.null(ignore)) {
-    x <- x[, intersect(colnames(x), ignore)]
-    y <- y[, intersect(colnames(y), ignore)]
-  }
 
   meta <- list()
   meta$names <- c(x = x_name, y = y_name)
   meta$dims <- purrr::map(list(x = x, y = y), ~ dim(.))
   meta$colnames <- purrr::map(list(x = x, y = y), ~ colnames(.))
   meta$coltypes <- purrr::map(list(x = x, y = y), ~ purrr::map_chr(., ~ class(.)[1]))
+
+  # Check number of rows and address
+  if (align || nrow(x) != nrow(y)) {
+    xy <- align_data_frames(x, y, group_vars = group_vars)
+    x <- xy$x
+    y <- xy$y
+  }
+
+  # Drop ignored columns
+  if (!is.null(ignore)) {
+    x <- x[, intersect(colnames(x), ignore)]
+    y <- y[, intersect(colnames(y), ignore)]
+  }
 
   # De-factorize into characters
   x <- mutate_if(x, is.factor, as.character)
@@ -34,36 +46,49 @@ tidy_diff <- function(x, y, ignore = NULL) {
   z <- full_join(xt, yt, by = "variable") %>%
     mutate(
       misses     = purrr::map2(value.x, value.y, ~ not_equal(..1, ..2)),
+      misses     = purrr::map2(misses, value.x, ~ union(..1, which(is.na(..2)))),
+      misses     = purrr::map2(misses, value.y, ~ union(..1, which(is.na(..2)))),
+      misses     = purrr::map(misses, sort),
       miss_count = purrr::map_int(misses, length),
       miss_count = ifelse(purrr::map_lgl(value.x, is.null), NA, miss_count),
       miss_count = ifelse(purrr::map_lgl(value.y, is.null), NA, miss_count),
       state      = ifelse(miss_count == 0, "same", "diff"),
       state      = ifelse(purrr::map_lgl(value.x, is.null), "unique_x", state),
-      state      = ifelse(purrr::map_lgl(value.y, is.null), "unique_y", state)
-      # state      = case_when(
-      #   miss_count > 0    ~ "diff",
-      #   is.na(miss_count) ~ "unique",
-      #   miss_count == 0   ~ "same",
-      #   TRUE ~ "other"
-      # )
+      state      = ifelse(purrr::map_lgl(value.y, is.null), "unique_y", state),
+      state      = ifelse(grepl("^_row\\.", variable), "same", state)             # Manually move row indices to "same" group
     ) %>%
     split(.$state)
 
-  # TODO: catch no changes
-  z_tidy_diff <- z$diff
-  z_tidy_diff$value.x <- purrr::map2(z$diff$value.x, z$diff$misses, function(x, y) x[y])
-  z_tidy_diff$value.y <- purrr::map2(z$diff$value.y, z$diff$misses, function(x, y) x[y])
-  z_tidy_diff <- z_tidy_diff %>%
-    select(-miss_count, -state) %>%
-    split(.$variable) %>%
-    purrr::map(~ {
-      tidyr::unnest(.) %>%
-        rename(miss_index = misses)
-    })
+  if (!is.null(z$diff)) {
+    z_tidy_diff <- z$diff
+    z_tidy_diff$value.x <- purrr::map2(z$diff$value.x, z$diff$misses, function(x, y) x[y])
+    z_tidy_diff$value.y <- purrr::map2(z$diff$value.y, z$diff$misses, function(x, y) x[y])
+    z_tidy_diff <- z_tidy_diff %>%
+      select(-miss_count, -state) %>%
+      split(.$variable) %>%
+      purrr::map(~ {
+        tidyr::unnest(.) %>%
+          rename(miss_index = misses)
+      })
 
-  z_tidy_diff <- purrr::map(z_tidy_diff, function(z_td) {
-    bind_cols(z_td, x[z_td$miss_index,  z$same$variable])
-  })
+    if (!is.null(z$same)) {
+      # Add common columns to z_tidy_diff (i.e. ids, etc.)
+      z_tidy_diff <- purrr::map(z_tidy_diff, function(z_td) {
+        bind_cols(z_td, x[z_td$miss_index,  z$same$variable])
+      })
+    }
+
+    if ("_row.x" %in% names(z_tidy_diff[[1]])) {
+      z_tidy_diff <- purrr::map(
+        z_tidy_diff,
+        ~ rename(., miss_index.x = `_row.x`, miss_index.y = `_row.y`) %>%
+          select(-`_row.z`) %>%
+          select(variable, starts_with("value"), starts_with("miss"), everything())
+      )
+    }
+  } else {
+    z_tidy_diff <- NULL
+  }
 
   z <- purrr::map_dfr(z, ~ {
     select(., variable, state, miss_count, misses)
@@ -99,6 +124,10 @@ print.tidy_diff <- function(z, n = 5) {
     )
 
   n <- min(length(z_tidy), n)
+  if (n == 0) {
+    cli::cat_line(glue::glue("There were no differences found bewtween {paste0('`', zs$meta$names, '`', collapse = ' and ')}"))
+    return(invisible())
+  }
   cli::cat_line(pillar::style_subtle(glue::glue("Showing differences in first {n} columns...\n\n")))
   for (i in 1:n) {
     print(z_tidy[[i]])
@@ -217,7 +246,10 @@ summary.tidy_diff <- function(z) {
     ))
   }
 
-  # TODO: Catch no changes
+  if (is.null(z$tidy)) {
+    cli::cat_bullet(glue::glue("No differences in the values between '{x_name}' and '{y_name}'"))
+    return(invisible())
+  }
   n_misses <- filter(z$diff, state == "diff") %>% pull(miss_count) %>% sum(na.rm = TRUE)
   n_miss_rows <- filter(z$diff, state == "diff") %>% pull(misses) %>% unlist() %>% unique() %>% length()
   trunc_bullet(glue::glue(
@@ -226,6 +258,7 @@ summary.tidy_diff <- function(z) {
   ))
   if (n_misses) {
     z$diff %>%
+      filter(!grepl("^_row\\.", variable)) %>%
       mutate(misses = purrr::map_chr(misses, paste, collapse = ", ")) %>%
       rename(`misses (row id)` = misses) %>%
       capture_tibble_print() %>%
@@ -234,5 +267,69 @@ summary.tidy_diff <- function(z) {
 
 }
 
+#' Align tibbles or data frames using grouping vars
+#'
+#' Uses `group_vars` to find missing rows in `x` or `y` and align final rows.
+align_data_frames <- function(x, y, group_vars = NULL) {
+  if (!"grouped_df" %in% union(class(x), class(y))) {
+    if (is.null(group_vars)) {
+      rlang::abort("`x` and `y` contain a different number of rows. Use `group_by()` to provide grouping variables to inform alignment.")
+    }
+  }
+  if (!is.null(group_vars)) {
+    # Use manually provided groups
+    x <- group_by(x, !!!rlang::syms(group_vars))
+    y <- group_by(y, !!!rlang::syms(group_vars))
+  } else {
+    # Use groups from grouped_df
+    group_vars <- purrr::map(list(x = x, y = y), dplyr::group_vars)
 
-test <- function(x, y) browser()
+    # Handle mismatched groups
+    if (length(unique(purrr::map_int(group_vars, length))) != 1) {
+      # If group vars are available in both, use union, else abort
+      group_vars_all <- purrr::reduce(group_vars, union)
+      if (length(setdiff(group_vars_all, union(names(x), names(y)))) == 0) {
+        group_vars <- list(group_vars_all)
+      } else {
+        common <- purrr::reduce(group_vars, intersect)
+        uniq   <- purrr::map(group_vars, setdiff, y = common) %>% purrr::compact()
+        msg    <- glue::glue(
+          "`x` and `y` have group column(s) ", glue::glue_collapse('"{common}"', sep = ", "),
+          " in common but ", glue::glue_collapse(
+            purrr::imap_chr(uniq, ~ glue::glue("`{.y}` has column(s) ", glue::glue_collapse('"{.x}"', sep = ", "))),
+            sep = " and "
+          )
+        )
+        rlang::abort(msg)
+      }
+    }
+    group_vars <- group_vars[[1]]
+  }
+
+  # Check duplicates in grouping vars
+  purrr::imap(list(x = x, y = y), ~ {
+    distinct_rows <- nrow(distinct(.x, !!!rlang::syms(group_vars)))
+    if (nrow(.x) != distinct_rows) {
+      rlang::abort(glue::glue("`{.y}` only has {distinct_rows} distinct rows out of {nrow(.x)} rows"))
+    }
+  })
+
+  # Add original row numbers
+  xy <- purrr::map(list(x = x, y  = y), ~ ungroup(.))
+  for (v in c("x", "y")) {
+    xy[[v]][[paste0("_row.", v)]] <- 1:nrow(xy[[v]])
+  }
+
+  # Join by IDs and generate new row numbers
+  row_ids <- xy %>%
+    purrr::map(~ select(., !!!rlang::syms(group_vars), starts_with("_row"))) %>%
+    purrr::reduce(full_join, by = group_vars) %>%
+    mutate(`_row.z` = 1:nrow(.))
+
+  # Merge old data frames with new row numbers, arrange by new row
+  xy %>%
+    purrr::map(
+      ~ full_join(., row_ids, by = intersect(names(.), names(row_ids))) %>%
+        arrange(`_row.z`)
+    )
+}
