3ae244d5a4180938b6e0a04cd8304863bfeffce1
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-07-13 11:20:13 -0400

    Add Moffitt color palette and CSS file

---
+++ R/grkmisc-package.R
@@ -0,0 +1,15 @@
+#' @keywords internal
+"_PACKAGE"
+
+
+#' Moffitt Colors
+#'
+#' Color codes for the colors listed in the Moffitt Brand Guide 2014.
+#'
+#' @section CSS:
+#' A CSS palette is included as well, that can be found via
+#' `system.file("moffitt-colors.css", package = "grkmisc")`.
+#'
+#' @format A named vector of 7 colors: blue, light_blue, green, orange,
+#'   grey, yellow, and red.
+"moffitt_colors"
---
+++ data-raw/moffitt_colors.R
@@ -0,0 +1,9 @@
+moffitt_colors <- c(
+  blue       = "#00589a",
+  light_blue = "#58b0e3",
+  green      = "#82c878",
+  orange     = "#faa555",
+  grey       = "#bec3c3",
+  yellow     = "#ffcd5a",
+  red        = "#eb1455"
+)
