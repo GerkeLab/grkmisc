03c190934e24c48f4c877bf967855ffd9c4ad82b
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-08-03 12:20:37 -0400

    Add use_git_hook_precommit() with default pre-commit hook

--- R/package.R
+++ R/package.R
@@ -5,12 +5,50 @@
 #' @param directory Directory for the .gitignore, defaults to project root.
 #' @param browse Open the .gitignore file after modifying?
 #' @export
-use_gitignore <- function(directory = NULL, browse = TRUE) {
-  if (is.null(directory)) directory <- here::here()
+use_gitignore <- function(directory = here::here(), browse = TRUE) {
   proj_gitignore <- file.path(directory, ".gitignore")
   default_gitignore <- system.file("templates", ".gitignore", package = "grkmisc")
   append <- file.exists(proj_gitignore)
-  if (append) cli::cat_line("Adding default .gitignore to existing file in ", directory)
   cat(readLines(default_gitignore), sep = "\n", file = proj_gitignore, append = append)
   if (browse && rstudioapi::isAvailable()) rstudioapi::navigateToFile(proj_gitignore)
+  if (append) {
+    cli::cat_bullet("Added default .gitignore lines to existing file in ", directory)
+  } else cli::cat_bullet("Added default .gitignore in ", directory)
+  return(invisible())
+}
+
+#' Use grkmisc git Pre-Commit Hook
+#'
+#' Installs a pre-commit hook that automatically bumps the lowest part of the
+#' package version before committing - for example from `0.1.2.9000` to
+#' `0.1.2.90001` - and tidies up the DESCRIPTION file using
+#' [usethis::use_tidy_description]. If the version was manually changed as part
+#' of the commit, the version number is not changed. If changes were made to the
+#' DESCRIPTION file but this file was not committed, those additional changes
+#' will be committed with the version bump.
+#'
+#' @section Disabling the hook:
+#' You can disable the pre-commit hook in two ways. First you can set an
+#' environment variable when committing your files:
+#' `doIncrement=FALSE git commit -m "commit message"`. Alternatively, you can
+#' skip the pre-commit hook with `git commit --no-verify -m "commit message"`.
+#'
+#' @param directory Root directory containing the git repository
+#' @export
+use_git_hook_precommit <- function(directory = here::here()) {
+  if (!dir.exists(file.path(directory, ".git"))) rlang::abort(
+    "This project is not contained in a git repository. You may need to manually specify the `directory`."
+  )
+  git_hooks_dir <- file.path(directory, ".git", "hooks")
+  git_precommit_file <- file.path(git_hooks_dir, "pre-commit")
+  if (file.exists(git_precommit_file)) {
+   cli::cat_bullet(
+     aste("A pre-commit hook already exists in", git_hooks_dir),
+     bullet = "cross", bullet_col = "red")
+    return(invisible())
+  }
+  file.copy(system.file("templates", "pre-commit.R", package = "grkmisc"),
+            git_precommit_file)
+  cli::cat_bullet("Added grkmisc pre-commit hook")
+  return(invisible())
 }
---
+++ inst/templates/pre-commit.R
@@ -0,0 +1,56 @@
+#!/usr/bin/env Rscript
+
+# License: CC0 (just be nice and point others to where you got this)
+# Author: Robert M Flight <rflight79@gmail.com>, github.com/rmflight
+#
+# This is a pre-commit hook that checks that there are files to be committed, and if there are, increments the package version
+# in the DESCRIPTION file.
+#
+# To install it, simply copy this into the ".git/hooks/pre-commit" file of your git repo, change /path/2/Rscript, and make it
+# executable. Note that /path/2/Rscript is the same as your /path/2/R/bin/R, or may be in /usr/bin/Rscript depending on your
+# installation. This has been tested on both Linux and Windows installations.
+#
+# In instances where you do NOT want the version incremented, add the environment variable doIncrement=FALSE to your git call.
+# eg "doIncrement=FALSE git commit -m "commit message"".
+# This is useful when you change the major version number for example.
+
+doIncrement <- TRUE # default
+
+# get the environment variable and modify if necessary
+tmpEnv <- as.logical(Sys.getenv("doIncrement"))
+if (!is.na(tmpEnv)){
+  doIncrement <- tmpEnv
+}
+
+# check that there are files that will be committed, don't want to increment version if there won't be a commit
+fileDiff <- system("git diff HEAD --name-only", intern=TRUE)
+
+if ("DESCRIPTION" %in% fileDiff) {
+  # Don't want to overwrite manual version bump
+  desc_diff <- system("git diff HEAD DESCRIPTION", intern = TRUE)
+  doIncrement <- !any(grepl("\\+Version", desc_diff))
+  staged_files <- system("git diff HEAD --name-only --staged", intern = TRUE)
+  desc_staged <- "DESCRIPTION" %in% staged_files
+  if (!desc_staged) {
+    if (doIncrement) cat("DESCRIPTION had additional changes that were committed.\n")
+    else cat("DESCRIPTION contains manual version bump but was not staged, so it was not committed.\n")
+  }
+}
+
+if ((length(fileDiff) > 0) && doIncrement){
+  currDir <- getwd() # this should be the top level directory of the git repo
+  currDCF <- read.dcf("DESCRIPTION")
+  currVersion <- currDCF[1,"Version"]
+  splitVersion <- strsplit(currVersion, ".", fixed=TRUE)[[1]]
+  nVer <- length(splitVersion)
+  currEndVersion <- as.integer(splitVersion[nVer])
+  newEndVersion <- as.character(currEndVersion + 1)
+  splitVersion[nVer] <- newEndVersion
+  newVersion <- paste(splitVersion, collapse=".")
+  currDCF[1,"Version"] <- newVersion
+  if ("Date" %in% colnames(currDCF)) currDCF[1, "Date"] <- strftime(as.POSIXlt(Sys.Date()), "%Y-%m-%d")
+  write.dcf(currDCF, "DESCRIPTION")
+  if (requireNamespace("usethis", quietly = TRUE)) usethis::use_tidy_description()
+  system("git add DESCRIPTION")
+  cat("Incremented package version", currVersion, "-->", newVersion, "and added to commit!\n")
+}
