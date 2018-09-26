8f81903384014266b72f5dced511cf0ef01ebabd
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-09-26 13:10:47 -0400

    Add insert_path_dir_active() and _abs()

--- R/style_console.R
+++ R/style_console.R
@@ -37,3 +37,26 @@ insert_styled_text <- function(x = NULL, ...) {
   context <- rstudioapi::getSourceEditorContext()
   rstudioapi::insertText(context$selection[[1]]$range, x, context$id)
 }
+
+#' Insert Path Containing Active File
+#'
+#' For the "Insert Relative Directory Path" RStudio Addin. Inserts the relative
+#' path to the directory containing the active source document. If text is
+#' selected, this text is assumed to be a file inside that directory and is
+#' appended to the path. The "Insert Absolute Directory Path" Rstudio Addin does
+#' the same but provides the absolute path to the active source document.
+insert_path_dir_active <- function(relative = TRUE) {
+  context <- rstudioapi::getSourceEditorContext()
+  this_path <- if (relative) {
+    fs::path_rel(fs::path_dir(context$path), here::here())
+  } else {
+    fs::path_dir(fs::path_abs(context$path))
+  }
+  selected <- context$selection[[1]]
+  rstudioapi::insertText(selected$range, fs::path(this_path, selected$text), context$id)
+}
+
+#' @rdname insert_path_dir_active
+insert_path_dir_active_abs <- function() {
+  insert_path_dir_active(FALSE)
+}
