37c5c538f564129ff9f101b47296bcc077036a22
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-09-28 14:02:34 -0400

    Update colors and show colors in pkgdown

--- R/grkmisc-package.R
+++ R/grkmisc-package.R
@@ -10,6 +10,11 @@
 #' A CSS palette is included as well, that can be found via
 #' `system.file("moffitt-colors.css", package = "grkmisc")`.
 #'
+#' @examples
+#' str(moffitt_colors)
+#' scales::show_col(unlist(moffitt_colors))
+#'
+#' @seealso theme_moffitt
 #' @format A named list of 7 colors: blue, light_blue, green, orange,
 #'   grey, yellow, and red.
 "moffitt_colors"
--- data-raw/moffitt_colors.R
+++ data-raw/moffitt_colors.R
@@ -1,10 +1,10 @@
 moffitt_colors <- list(
   blue       = "#00589a",
-  light_blue = "#58b0e3",
   green      = "#82c878",
+  red        = "#eb1455",
   orange     = "#faa555",
-  grey       = "#bec3c3",
+  light_blue = "#58b0e3",
   yellow     = "#ffcd5a",
-  red        = "#eb1455"
+  grey       = "#bec3c3"
 )
 use_data(moffitt_colors, overwrite = TRUE)
