1affe5c04631cff8f13c5bde20e2be6dce0e6047
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-10-12 14:41:08 -0400

    Add radix template

--- R/themes.R
+++ R/themes.R
@@ -234,10 +234,11 @@ scale_fill_moffitt <- function(color_other = "grey", ...) {
 #' Create New Document
 #'
 #' Creates a new R Markdown document of the requested type. There are currently
-#' two templates, one for reports where the default is HTML based on the
-#' HTML vignette template, and another with a Moffitt-styled xaringan theme.
-#' In both cases, the document and supporting files are added to a directory
-#' with the name given by the file.
+#' three templates, one for reports where the default is HTML based on the HTML
+#' vignette template, and another with a Moffitt-styled [xaringan] theme. There
+#' is also a [radix] template for stunning HTML-only reports. In all cases, the
+#' document and supporting files are added to a directory with the name given by
+#' the file.
 #'
 #' @examples
 #' \dontrun{
@@ -248,15 +249,16 @@ scale_fill_moffitt <- function(color_other = "grey", ...) {
 #' @param path Path to the location of the new document
 #' @param type Type of document to create
 #' @export
-doc_new <- function(path, type = c("doc", "slides")) {
+doc_new <- function(path, type = c("doc", "slides", "radix")) {
   type <- match.arg(type)
   if (!dir.exists(dirname(path))) {
     dir.create(dirname(path), recursive = TRUE)
   }
   file_path <- switch(
     type,
-    "doc" = rmarkdown::draft(path, template = "default", package = "grkmisc", edit = FALSE),
-    "slides" = rmarkdown::draft(path, template = "moffitt-xaringan", package = "grkmisc", edit = FALSE)
+    "doc"    = rmarkdown::draft(path, template = "default", package = "grkmisc", edit = FALSE),
+    "slides" = rmarkdown::draft(path, template = "moffitt-xaringan", package = "grkmisc", edit = FALSE),
+    "radix"  = rmarkdown::draft(path, template = "grk-radix", package = "grkmisc", edit = FALSE)
   )
   if (requireNamespace("rstudioapi", quietly = TRUE))
     rstudioapi::navigateToFile(file_path) else file_path
