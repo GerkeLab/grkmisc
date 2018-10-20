19a3bf671a4cb49d7d73b05a392e6f0c26ba7c4a
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-10-20 09:26:49 -0400

    Add if_na.data.frame() and more refactoring

--- R/if_na.R
+++ R/if_na.R
@@ -1,6 +1,6 @@
-#' Replace NA values
+#' Replace Missing Values
 #'
-#' Replaces NA values with the given value, or optionally conditions the
+#' Replaces missing values with the given value, or optionally conditions the
 #' replacement on the expressions given in `...`, which are applied to the
 #' inputs in the same way as `filter`.
 #'
@@ -42,44 +42,110 @@
 #' @param ... Additional expressions to condition replacement. NA values are
 #'   only replaced when the additional expression matches.
 #' @export
-if_na <- function(x, ..., then = default_value(x), otherwise = NULL) {
+if_na <- function(.x, ..., then = default_value(x), otherwise = NULL) {
   UseMethod("if_na")
 }
 
 #' @export
-if_na.default <- function(x, ..., then = default_value(x), otherwise = NULL) {
-  then <- check_type(x, then)
-  if (!is.null(otherwise)) otherwise <- check_type(x, otherwise)
+if_na.default <- function(.x, ..., then = default_value(.x), otherwise = NULL) {
+  then <- check_type(.x, then)
+  if (!is.null(otherwise)) otherwise <- check_type(.x, otherwise)
 
-  conditions <- get_conditions(length(x), ...)
+  conditions <- get_conditions(length(.x), ...)
+
+  missing_and_conditions <- which(!is_complete(.x) & conditions)
+  .x[missing_and_conditions] <- if (length(then) > 1) {
+    stopifnot(length(then) == length(.x))
+    then[missing_and_conditions]
+  } else then
 
   if (!is.null(otherwise)) {
-    dplyr::if_else(
-      !is.na(x),
-      x,
-      dplyr::if_else(conditions, then, otherwise),
-    )
-  } else {
-    dplyr::if_else(!(is.na(x) & conditions), x, then)
+    missing_not_conditions <- which(!is_complete(.x) & !conditions)
+    .x[missing_not_conditions] <- if (length(otherwise) > 1) {
+      stopifnot(length(otherwise) == length(.x))
+      otherwise[missing_not_conditions]
+    } else otherwise
   }
+
+  .x
+}
+
+#' Replace Missing Values in Data Frames
+#'
+#' Replaces missing values in data frames, similar to [dplyr::replace_na()] with
+#' a few extras. There are several ways to use this function. If called without
+#' any arguments, all missing values will be replaced by the default type.
+#' You can change the global default value with the `then` argument, and you
+#' can subset the columns to be affected with the `vars` argument. Finally, you
+#' can specify column-wise replacement values as named arguments, in which case
+#' the other arguments are ignored.
+#'
+#' @examples
+#' df <- dplyr::tibble(
+#'   x = c(1, 2, NA), y = c("a", NA, "b"), z = list(1:5, NULL, 10:20)
+#' )
+#'
+#' df %>% if_na()
+#'
+#' df %>% if_na(then = "0")
+#'
+#' df %>% if_na(vars = vars(x, y))
+#'
+#' df %>% if_na(x = 3, y = "c")
+#'
+#' @inheritParams if_na
+#' @param ... Optional named arguments specifying column-wise replacement values
+#'   for missing values. Other arguments are ignored by `if_na()` when used.
+#' @param then Global replacement value to be used in place of the default.
+#' @param otherwise Ignored.
+#' @param vars Optional vector of column names that should receive the missing
+#'   value replacement. Use the `vars()` helper to provide bare column names.
+#' @export
+if_na.data.frame <- function(.x, ..., then = NULL, otherwise = NULL, vars = NULL) {
+  # otherwise is ignored
+  # then is ignored if ... are provided
+  vals <- list(...)
+  if (length(vals)) {
+    replace_vars <- intersect(names(vals), names(.x))
+    if (!is.null(then))
+      rlang::warn("`then` is ignored when values are provided in `...`")
+  } else {
+    replace_vars <- names(.x)
+    if (!is.null(vars)) {
+      vars <- get_vars(vars)
+      replace_vars <- intersect(vars, replace_vars)
+    }
+    vals <- if (is.null(then)) {
+      purrr::set_names(replace_vars) %>%
+        purrr::map(function(v) default_value(.x[[v]]))
+    } else {
+      purrr::set_names(replace_vars) %>%
+        purrr::map(~ then)
+    }
+  }
+  for (var in replace_vars) {
+    .x[[var]] <- if_na(.x[[var]], then = vals[[var]])
+  }
+
+  .x
 }
 
 #' @export
-if_na.factor <- function(x, ..., then = "(Missing)", otherwise = NULL) {
+if_na.factor <- function(.x, ..., then = "(Missing)", otherwise = NULL) {
   # if_na() for factors uses forcats::fct_explicit_na()
   # which does not work with the if_else() workflow
   then <- as.character(then)
   otherwise <- if (!is.null(otherwise)) as.character(otherwise)
 
-  conditions <- get_conditions(length(x), ...)
+  conditions <- get_conditions(length(.x), ...)
 
   if (!is.null(otherwise)) {
-    conditions_else <- is.na(x) & !conditions
-    x <- forcats::fct_explicit_na(x, then)
-    x[conditions_else] <- NA
-    forcats::fct_explicit_na(x, otherwise)
+    conditions_else <- is.na(.x) & !conditions
+    .x <- forcats::fct_explicit_na(.x, then)
+    .x[conditions_else] <- NA
+    forcats::fct_explicit_na(.x, otherwise)
   } else {
-    forcats::fct_explicit_na(x, then)
+    forcats::fct_explicit_na(.x, then)
   }
 }
 
@@ -90,6 +156,30 @@ get_conditions <- function(n, ...) {
   } else rep(TRUE, n)
   stopifnot(length(conditions) == n)
   conditions
+}
+
+is_complete <- function(x) {
+  # tidyr:::is_complete
+  if (typeof(x) == "list") {
+    !purrr::map_lgl(x, purrr::is_empty)
+  } else {
+    !is.na(x)
+  }
+}
+
+vars <- function(...) {
+  rlang::quos(...)
+}
+
+get_vars <- function(x) {
+  if (inherits(x, "quosures")) {
+    x <- purrr::map_chr(x, rlang::quo_text)
+    return(x)
+  } else if (inherits(x, "character")) {
+    return(x)
+  } else {
+    rlang::abort("Please use provide variable names in vars or use the vars() helper.")
+  }
 }
 
 default_value <- function(x) UseMethod("default_value")
@@ -115,6 +205,8 @@ check_type <- function(x, value) {
       # apparently as(1L, "numeric") doesn't work...
       # https://twitter.com/grrrck/status/1053330846856429568
       as.double(value)
+    } else if (inherits(x, "list")) {
+      list(value)
     } else {
       as(value, class(x))
     }
--- tests/testthat/test-if_na.R
+++ tests/testthat/test-if_na.R
@@ -19,10 +19,13 @@ test_that("if_na() works", {
 
   storms_3 <- storms %>%
     dplyr::mutate(ts_diameter =
-      if_na(ts_diameter, then = wind, status == "tropical storm", otherwise = 0)
+      if_na(ts_diameter, then = wind, status == "tropical storm", otherwise = pressure)
     )
   storms_4 <- storms %>%
-    dplyr::mutate(ts_diameter = ifelse(status == "tropical storm", wind, 0))
+    dplyr::mutate(
+      ts_diameter = dplyr::if_else(status == "tropical storm", wind, pressure),
+      ts_diameter = as.double(ts_diameter)
+    )
 
   expect_equal(storms_3, storms_4)
 
@@ -36,10 +39,46 @@ test_that("if_na() works", {
     if_na(factor(1:4, 1:2), c(T, T, T, F), then = 10, otherwise = 11),
                factor(c(1L, 2L, 10L, 11L))
   )
-  expect_equal(if_na(list(NA, 1, list(2))), list(NULL, 1, list(2)))
+  expect_equal(if_na(list(NULL, 1, list(2)), then = 3), list(3, 1, list(2)))
   expect_equal(suppressWarnings(if_na(c(1L, NA_integer_), then = "a")), c(1, NA_integer_))
   expect_warning(if_na(c(1L, NA_integer_), then = "a"))
   expect_equal(if_na(c(TRUE, NA)), c(TRUE, FALSE))
   expect_error(if_na(structure(1L, class = "unknown")), "Please provide")
   expect_error(if_na(structure(1L, class = "unknown"), then = 0), "Unable to coerce")
+  expect_error(if_na(c(1, NA, 2), then = c(1, 2)))
+  expect_error(if_na(c(1, NA, 2), c(T, F, T), otherwise = c(1, 2)))
+})
+
+test_that("if_na() works with data frames", {
+  df <- dplyr::tibble(
+    x = c(1, 2, NA), y = c("a", NA, "b"), z = list(1:5, NULL, 10:20)
+  )
+
+  x <- df %>% if_na()
+  expect_equal(x$x, c(1, 2, 0))
+  expect_equal(x$y, c("a", "", "b"))
+  expect_equal(x$z, list(1:5, NULL, 10:20))
+
+  x <- df %>% if_na(then = "0")
+  expect_equal(x$x, c(1, 2, 0))
+  expect_equal(x$y, c("a", "0", "b"))
+  expect_equal(x$z, list(1:5, "0", 10:20))
+
+  x <- df %>% if_na(vars = vars(x, y))
+  expect_equal(x$x, c(1, 2, 0))
+  expect_equal(x$y, c("a", "", "b"))
+  expect_equal(x$z, list(1:5, NULL, 10:20))
+
+  x <- df %>% if_na(vars = c("x", "y"))
+  expect_equal(x$x, c(1, 2, 0))
+  expect_equal(x$y, c("a", "", "b"))
+  expect_equal(x$z, list(1:5, NULL, 10:20))
+
+  x <- df %>% if_na(x = 3, y = "c")
+  expect_equal(x$x, c(1, 2, 3))
+  expect_equal(x$y, c("a", "c", "b"))
+  expect_equal(x$z, list(1:5, NULL, 10:20))
+
+  expect_warning(if_na(x, x = 3, then = 5))
+  expect_error(if_na(x, vars = 1:2))
 })
