6e8a8966ec1e97490ac23f2d14d14e72639f9fa9
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-10-23 12:25:13 -0400

    Add pipe-capable logger function

--- R/misc.R
+++ R/misc.R
@@ -59,3 +59,59 @@ prettify_number <- function(x, units = c('k' = 1000, 'M' = 1e6, "B" = 1e9), deci
 recode_if <- function(x, condition, ...) {
   dplyr::if_else(condition, dplyr::recode(x, ...), x)
 }
+
+
+#' Pipe-Capable Log Messages
+#'
+#' Writes a log message, suitable for use inside pipes, meaning that the first
+#' argument is passed through untouched. The log message is processed using
+#' [glue::glue], and you can reference the incoming data as `.data` inside the
+#' log message.
+#'
+#' @section Options:
+#'
+#'   You can set the maximum debug level and the log output locations using the
+#'   global options `grkmisc.log_level` and `grkmisc.log_output`. The defaults
+#'   are `"INFO"` and `stdout()` respectively.
+#'
+#' @examples
+#' library(dplyr)
+#' iris %>%
+#'   logger("Starting with {nrow(.data)} rows") %>%
+#'   filter(Species == "setosa") %>%
+#'   logger("Filtered to {nrow(.data)} rows and {ncol(.data)} columns") %>%
+#'   head()
+#'
+#' @param .data This argument is returned as-is.
+#' @param msg The log message, processed using [glue::glue]. You can refernce
+#'   the data passed in via `.data`.
+#' @param level The log level, default is "INFO" and possible values include
+#'   "DEBUG", "INFO", "WARNING", "ERROR", and "FATAL".
+#' @export
+logger <- function(.data = NULL, msg = "", level = "INFO") {
+  level <- logger_level(level)
+  if (level < logger_level(getOption("grkmisc.log_level", "INFO"))) {
+    return(invisible(.data))
+  }
+
+  msg <- glue::glue(msg)
+  cli::cat_line(
+    strftime(Sys.time(), "[%F %H:%M:%OS6] "),
+    sprintf("%-8s", names(level)),
+    msg,
+    file = getOption("grkmisc.log_output", stdout())
+  )
+  .data
+}
+
+logger_level <- function(level) {
+  levels <- c(
+    "DEBUG"   = 0,
+    "INFO"    = 1,
+    "WARNING" = 2,
+    "ERROR"   = 3,
+    "FATAL"   = 4
+  )
+  level <- match.arg(toupper(level), names(levels), several.ok = FALSE)
+  levels[level]
+}
