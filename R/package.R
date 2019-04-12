9750bafd0857c79cdb490a69414dd210419584a5
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2019-04-12 13:03:52 -0400

    Add use_grk_github_labels()

--- R/package.R
+++ R/package.R
@@ -192,3 +192,69 @@ in_rstudio_project <- function() {
 is_usethis_recent <- function() {
   package_version(packageVersion("usethis")) >= "1.4.0.9000"
 }
+
+#' Setup GerkeLab Default GitHub Labels
+#'
+#' Removes the default GitHub labels and installs the GerkeLab labels. A small
+#' wrapper around [usethis::use_github_labels()].
+#'
+#' @inheritDotParams usethis::use_github_labels
+#' @inheritParams usethis::use_github_labels
+#' @export
+use_grk_github_labels <- function(
+  ...,
+  labels = grk_github_labels(),
+  colours = grk_github_colours(),
+  descriptions = grk_github_descriptions(),
+  delete_default = TRUE
+) {
+  usethis::use_github_labels(
+    ...,
+    labels = labels,
+    colours = colours,
+    descriptions = descriptions,
+    delete_default = delete_default
+  )
+}
+
+#' @describeIn use_grk_github_labels Default grkmisc GitHub labels.
+#' @export
+grk_github_labels <- function() {
+  c(
+    "planning :seedling:",
+    "feature :hatching_chick:",
+    "question :question:",
+    "documentation :memo:",
+    "bug :bug:",
+    "duplicate",
+    ":construction: WIP :construction:"
+  )
+}
+
+#' @describeIn use_grk_github_labels Default grkmisc GitHub colours.
+#' @export
+grk_github_colours <- function() {
+  c(
+    "planning :seedling:" = "BFDADC",
+    "feature :hatching_chick:" = "B5DDA4",
+    "question :question:" = "99ADC2",
+    "documentation :memo:" = "3E363F",
+    "bug :bug:" = "754668",
+    "duplicate" = "CFD3D7",
+    ":construction: WIP :construction:" = "FBCA04"
+  )
+}
+
+#' @describeIn use_grk_github_labels Default grkmisc GitHub descriptions.
+#' @export
+grk_github_descriptions <- function() {
+  c(
+    "planning :seedling:" = "Development planning",
+    "feature :hatching_chick:" = "New functionality!",
+    "question :question:" = "Need help or more information",
+    "documentation :memo:" = "Documentation improvement, clarification, or update",
+    "bug :bug:" = "Something isn't working",
+    "duplicate" = "This issue or pull request already exists",
+    ":construction: WIP :construction:" = "Work in Progress! Don't Merge!"
+  )
+}
